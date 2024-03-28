e4cdeacbc7ca31f1f48f115df4409a2a69380c00
Osama <elsharqawy2@gmail.com>
2024-03-28 19:58:36 +0200

    Binary Trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,18 +11,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t iNodes = 0;
 
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
+		iNodes += (tree->right || tree->left) ? 1 : 0;
+		iNodes += binary_tree_nodes(tree->left);
+		iNodes += binary_tree_nodes(tree->right);
 	}
 
-	if ((tree->right == NULL && tree->left != NULL) || (tree->left == NULL && tree->right != NULL) || tree->parent == NULL)
-	{
-		iNodes++;
-	}
-
-	iNodes += binary_tree_nodes(tree->left);
-	iNodes += binary_tree_nodes(tree->right);
-	
 	return (iNodes);
 }
