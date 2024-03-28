b6de95f64d2b0c62a3cc1ff8619dd72aadb4bacb
Osama <elsharqawy2@gmail.com>
2024-03-28 16:42:07 +0200

    Binary Trees

--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,9 @@ void binary_tree_delete(binary_tree_t *tree);
 /* Check if a node is a leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* Checkk if a node is a root */
+int binary_tree_is_root(const binary_tree_t *node);
+
 /*** End of Functions Declarations ***/
 
 #endif /* __BINARY_TREES_H__ */
